a329d4d1505be4196475e77e12da611dfb92bd04
xavierbrasher <xavier.brasher@gmail.com>
2022-10-14 11:02:11 +0800

    initial commit

---
+++ xavier-movie-client/src/App.tsx
@@ -0,0 +1,11 @@
+import { useState } from 'react'
+import reactLogo from './assets/react.svg'
+import './App.css'
+
+function App() {
+return (
+        <h1 classname="text-3xl"> WOW </h1> 
+)
+}
+
+export default App
---
+++ xavier-movie-server/index.ts
@@ -0,0 +1,22 @@
+import dotenv from "dotenv";
+dotenv.config();
+
+import express from "express";
+import cors from "cors";
+import path from "path";
+
+const port = 80;
+const app = express();
+
+app.use(cors());
+app.use(express.json());
+app.use(express.urlencoded({ extended: false }));
+app.use(express.static(path.resolve("public/")));
+
+app.get("/", (req, res) => {
+    res.status(200).send("WOW")
+})
+
+app.listen(port, () => {
+    console.log("Portfolio is listening on port: " + port);
+})
